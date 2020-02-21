857f1535b2cbfd2c15bda907eb4f5f939b379e19
Anton Epaneshnikov <watskysh@gmail.com>
2020-02-22 00:19:44 +0600

    #1.implementation-task0 Реализация задачи 1. Вывод имени, возраста, строки символов.

---
+++ homework2/task1/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anton
+ * Date: 22.02.2020
+ * Time: 0:10
+ */
+
+$name = "Антон";
+$age = "26";
+
+echo "Меня зовут: " . $name . "<br>";
+echo "Мне " . $age . " лет" . "<br>";
+echo "\"!|/'\"\\";
