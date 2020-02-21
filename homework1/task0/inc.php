accc8a1ebc7598267675eed19e53a835048ee9f4
Anton Epaneshnikov <watskysh@gmail.com>
2020-02-22 00:04:35 +0600

    #0.implementation-task0 Реализация задачи 0. Правка кода по стандарту PSR-2

---
+++ homework1/task0/inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anton
+ * Date: 21.02.2020
+ * Time: 23:39
+ */
+
+const TEST_CONSTANTA = 'asd';
+
+function showSomething()
+{
+    return 'SOMETHING';
+}
---
+++ homework1/task0/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anton
+ * Date: 21.02.2020
+ * Time: 23:49
+ */
+
+$user_name = "Igor";
+
+if (1 == 1) {
+    echo 'hi';
+}
