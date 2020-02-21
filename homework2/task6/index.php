fac15bc518fbe5eb2a794af9f43f3667dcd84d29
Anton Epaneshnikov <watskysh@gmail.com>
2020-02-22 01:49:51 +0600

    #6.implementation-task6 Реализация таблицы умножения, с выделением результатов.

---
+++ homework2/task6/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anton
+ * Date: 22.02.2020
+ * Time: 1:31
+ */
+
+echo "<table border='2'>";
+
+for ($r = 1; $r <= 10; $r++) {
+    echo '<tr>';
+    for ($d = 1; $d <= 10; $d++) {
+        $res = $r * $d;
+        if ($d % 2 == 0 && $r % 2 == 0)
+            echo "<td style='padding: 10px'> (" . $res . ") </td>";
+        elseif ($d % 2 != 0 && $r % 2 != 0 && $d != 1 && $r != 1)
+            echo "<td style='padding: 10px'> [" . $res . "] </td>";
+        else
+            echo "<td style='padding: 10px'> " . $res . " </td>";
+    }
+}
+
+echo "</table>";
