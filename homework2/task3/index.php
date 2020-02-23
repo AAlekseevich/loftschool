4b3fd8124bfb80186b92e3bc8c8674c9f17f266b
Anton Epaneshnikov <watskysh@gmail.com>
2020-02-23 22:17:29 +0600

    #16.hw2.task3 Рефакторинг кода

--- homework2/task3/index.php
+++ homework2/task3/index.php
@@ -7,7 +7,6 @@
  */
 
 const MIN_LOWER_AGE = 1;
-const MAX_LOWER_AGE = 17;
 const MIN_UPPER_AGE = 18;
 const MAX_UPPER_AGE = 65;
 
@@ -17,7 +16,7 @@ if ($age >= MIN_UPPER_AGE && $age <= MAX_UPPER_AGE) {
     echo "Вам еще работать и работать";
 } elseif ($age > MAX_UPPER_AGE) {
     echo "Вам пора на пенсию";
-} elseif ($age >= MIN_LOWER_AGE && MAX_LOWER_AGE <= 17) {
+} elseif ($age >= MIN_LOWER_AGE && $age < MIN_UPPER_AGE) {
     echo "Вам ещё рано работать";
 } else {
     echo "Неизвестный возраст";
